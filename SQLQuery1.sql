ab8fd7df0648e7026eb21dde22e4f2c2c15177c4
dspchanuja@students.nsbm.ac.lk <dspchanuja@students.nsbm.ac.lk>
2024-04-30 22:42:31 +0530

    db update

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblAttendence] (
+    [EmpId]    VARCHAR (50)  NULL,
+    [Date]     VARCHAR (50)  NULL,
+    [Id]       VARCHAR (100) NULL,
+    [ClockIn]  VARCHAR (50)  NULL,
+    [ClockOut] VARCHAR (50)  NULL,
+    [Status]   VARCHAR (50)  NULL,
+    [Number]   VARCHAR (50)  NULL
+);
+
