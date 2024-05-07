8a260bd039b645df4b52e58ff04deb6cb495f578
PURNIMA <PURNIMA@LAPTOP-CBGO7598>
2024-05-08 01:49:19 +0530

    Shift Mgt Complete

--- pulzz/Backend/Back-Shifts.Designer.cs
+++ pulzz/Backend/Back-Shifts.Designer.cs
@@ -28,145 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label12 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.txtDate = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmployeeName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label12
+            // button1
             // 
-            this.label12.AutoSize = true;
-            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label12.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label12.Location = new System.Drawing.Point(353, 41);
-            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(17, 18);
-            this.label12.TabIndex = 28;
-            this.label12.Text = "X";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(91, 126);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(71, 24);
-            this.label11.TabIndex = 27;
-            this.label11.Text = "Shift By";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(89, 286);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(73, 24);
-            this.label10.TabIndex = 26;
-            this.label10.Text = "Shift To\r\n";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(255, 364);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(20, 16);
-            this.label6.TabIndex = 25;
-            this.label6.Text = "ID";
+            this.button1.Location = new System.Drawing.Point(56, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(92, 364);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(256, 263);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(91, 16);
-            this.label7.TabIndex = 24;
-            this.label7.Text = "Employee ID :";
+            this.label7.Size = new System.Drawing.Size(36, 16);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Date";
             // 
-            // label8
+            // txtDate
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(255, 329);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(46, 16);
-            this.label8.TabIndex = 23;
-            this.label8.Text = "NAME";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(90, 329);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(115, 16);
-            this.label9.TabIndex = 22;
-            this.label9.Text = "Employee Name :";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(253, 197);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(20, 16);
-            this.label4.TabIndex = 21;
-            this.label4.Text = "ID";
+            this.txtDate.AutoSize = true;
+            this.txtDate.Location = new System.Drawing.Point(53, 263);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(36, 16);
+            this.txtDate.TabIndex = 16;
+            this.txtDate.Text = "Date";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(91, 197);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(248, 204);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(91, 16);
-            this.label5.TabIndex = 20;
-            this.label5.Text = "Employee ID :";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Name";
             // 
-            // label3
+            // txtEmployeeName
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(253, 163);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 16);
-            this.label3.TabIndex = 19;
-            this.label3.Text = "NAME";
+            this.txtEmployeeName.AutoSize = true;
+            this.txtEmployeeName.Location = new System.Drawing.Point(53, 204);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(109, 16);
+            this.txtEmployeeName.TabIndex = 14;
+            this.txtEmployeeName.Text = "Employee Name";
+            this.txtEmployeeName.Click += new System.EventHandler(this.txtEmployeeName_Click);
             // 
-            // label2
+            // label4
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(91, 163);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 16);
-            this.label2.TabIndex = 18;
-            this.label2.Text = "Employee Name :";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(248, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "ID";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.AutoSize = true;
+            this.txtEmployeeID.Location = new System.Drawing.Point(53, 126);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(85, 16);
+            this.txtEmployeeID.TabIndex = 12;
+            this.txtEmployeeID.Text = "Employee ID";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(110, 41);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(53, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 31);
-            this.label1.TabIndex = 17;
-            this.label1.Text = "Shift Approval\r\n";
-           
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Shift Rejected";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Back_Shifts
             // 
@@ -174,22 +119,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(245)))), ((int)(((byte)(250)))));
             this.ClientSize = new System.Drawing.Size(1312, 769);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDate);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmployeeID);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Back_Shifts";
             this.Text = "v";
-           
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,17 +137,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label txtDate;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txtEmployeeName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label txtEmployeeID;
         private System.Windows.Forms.Label label1;
     }
 }
--- pulzz/Backend/Back-Shifts.cs
+++ pulzz/Backend/Back-Shifts.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,47 @@ namespace pulzz.Backend
         public Back_Shifts()
         {
             InitializeComponent();
+        }
+
+        private void txtEmployeeName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //Declaring variables to add data
+            string EmployeeID = txtEmployeeID.Text;
+            string EmployeeName = txtEmployeeName.Text;
+            string Date = txtDate.Text;
+            //Creating the SQL query
+            string query = $"INSERT INTO Shift Rejected ('{EmployeeID}','{EmployeeName}','{Date}');";
+
+            //Creating the SQL command
+            SqlCommand cmd = new SqlCommand(query, conn);
+            //Executing the SQL command
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("SQL Command Execution Successful", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Close the ShiftApprovedForm
+            this.Close();
+            // Optionally, show the main form again
+            // Assuming Form1 is the name of your main form
+            Form1 mainForm = new Form1();
+            mainForm.Show();
         }
     }
 }
